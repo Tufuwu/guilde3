ce64154ad9f39ad86c56ec79189c76a38cddc0f9
wu <wu@.com>
2025-09-06 17:34:47 +0800

    run tenable/pytenable/gpt-4o-4

--- setup.py
+++ setup.py
@@ -1,67 +1,57 @@
-#!/usr/bin/env python
-# Copyright Jonathan Hartley 2013. BSD 3-Clause license, see LICENSE file.
-
-from __future__ import with_statement
-
-from io import open
+from setuptools import setup, find_packages
 import os
-import re
+
+with open('tenable/version.py', 'r') as vfile:
+    exec(vfile.read())
+
 try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-
-NAME = 'colorama'
-
-
-def read_file(path, encoding='ascii'):
-    with open(os.path.join(os.path.dirname(__file__), path),
-              encoding=encoding) as fp:
-        return fp.read()
-
-def _get_version_match(content):
-    # Search for lines of the form: # __version__ = 'ver'
-    regex = r"^__version__ = ['\"]([^'\"]*)['\"]"
-    version_match = re.search(regex, content, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
-
-def get_version(path):
-    return _get_version_match(read_file(path))
+    long_description = open(
+        os.path.join(
+            os.path.abspath(os.path.dirname(__file__)),
+            'README.rst')).read()
+except:
+    long_description = 'Please refer to https://pytenable.readthedocs.io'
+    print('! could not read README.rst file.')
 
 setup(
-    name=NAME,
-    version=get_version(os.path.join('colorama', '__init__.py')),
-    description='Cross-platform colored terminal text.',
-    long_description=read_file('README.rst'),
-    keywords='color colour terminal text ansi windows crossplatform xplatform',
-    author='Jonathan Hartley',
-    author_email='tartley@tartley.com',
-    maintainer='Arnon Yaari',
-    url='https://github.com/tartley/colorama',
-    license='BSD',
-    packages=[NAME],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
-    # see classifiers https://pypi.org/pypi?%3Aaction=list_classifiers
+    name='pyTenable',
+    version=version,
+    description='Python library to interface into Tenable\'s products and applications',
+    author='Tenable, Inc.',
+    long_description=long_description,
+    author_email='smcgrath@tenable.com',
+    url='https://github.com/tenable/pytenable',
+    license='MIT',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Terminals',
-    ]
+    ],
+    keywords='tenable tenable_io securitycenter containersecurity',
+    packages=find_packages(exclude=['docs', 'tests']),
+    install_requires=[
+        'requests>=2.19',
+        'python-dateutil>=2.6',
+        'semver>=2.8.1',
+        'ipaddress>=1.0.22',
+        'restfly>=1.3.5',
+        'marshmallow>=3.6',
+        'python-box>=4.0',
+    ],
+    extras_require={
+        'NessusReportv2': ['defusedxml>=0.5.0'],
+        'PWCertAuth': ['requests-pkcs12>=1.3'],
+        'docker': ['docker>=3.7.2'],
+        'complete': [
+            'defusedxml>=0.5.0',
+            'requests-pkcs12>=1.3',
+            'docker>=3.7.2',
+        ]
+    }
 )
