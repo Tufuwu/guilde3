cde775a07db01abb472b878388e0d87c6e047fad
wu <wu@.com>
2025-09-06 17:27:01 +0800

    run opensanctions/fingerprints/gpt-4o-4

--- setup.py
+++ setup.py
@@ -1,32 +1,45 @@
-import sys
 from setuptools import setup, find_packages
 
+
 setup(
-    name = "python-dispatch",
-    version = "v0.1.31",
-    author = "Matthew Reid",
-    author_email = "matt@nomadic-recording.com",
-    description = "Lightweight Event Handling",
-    url='https://github.com/nocarryr/python-dispatch',
-    license='MIT',
-    packages=find_packages(exclude=['tests*']),
-    include_package_data=True,
-    keywords='event properties dispatch',
-    platforms=['any'],
-    classifiers = [
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Software Development',
-        'Programming Language :: Python :: 2',
+    name='fingerprints',
+    version='0.6.6',
+    description="A library to generate entity fingerprints.",
+    long_description="",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
     ],
+    keywords='names people companies normalisation',
+    author='Friedrich Lindenberg',
+    author_email='friedrich@pudo.org',
+    url='http://github.com/alephdata/fingerprints',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'test']),
+    namespace_packages=[],
+    package_data={
+        '': ['fingerprints/data/types.yml']
+    },
+    include_package_data=True,
+    zip_safe=False,
+    test_suite='nose.collector',
+    install_requires=[
+        'normality>=0.4.0',
+        'pyyaml',
+        'six'
+    ],
+    tests_require=[
+        'nose',
+        'coverage',
+        'wheel'
+    ],
+    entry_points={
+    }
 )
